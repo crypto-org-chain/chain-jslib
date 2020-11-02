ff001e8170e1c4d821630bab23da7c0646f7b9af
Eddy <eddy.wanny@crypto.com>
2020-11-02 10:23:01 +0300

    Initial refactoring on new approach

--- lib/src/coin/coin.spec.ts
+++ lib/src/coin/coin.spec.ts
@@ -1,23 +1,22 @@
 import { expect } from 'chai';
 import { fuzzyDescribe } from '../test/mocha-fuzzy/suite';
 
-import { Coin } from './coin';
-import { CroSDK } from '../core/cro';
+import { CroSDK, croSDK } from '../core/cro';
 
-const cro = new CroSDK({ network: CroSDK.Testnet });
+const cro = croSDK({ network: CroSDK.Testnet });
 
 describe('Coin', function () {
     describe('constructor', function () {
         fuzzyDescribe('should throw Error when the provided argument is not (string, Unit)', function (fuzzy) {
-            const testRunner = fuzzy(fuzzy.StringArg('1000'), fuzzy.StringArg(Coin.UNIT_BASE));
+            const testRunner = fuzzy(fuzzy.StringArg('1000'), fuzzy.StringArg(cro.coin.Coin.UNIT_BASE));
             testRunner(
                 function (args0, args1) {
                     if (!args0.valid) {
-                        expect(() => cro.Coin(args0.value, args1.value)).to.throw(
+                        expect(() => new cro.coin.Coin(args0.value, args1.value)).to.throw(
                             'Expected `amount` to be of type `string`',
                         );
                     } else if (!args1.valid) {
-                        expect(() => cro.Coin(args0.value, args1.value)).to.throw(
+                        expect(() => new cro.coin.Coin(args0.value, args1.value)).to.throw(
                             'Expected `unit` to be of type `string`',
                         );
                     }
@@ -28,34 +27,38 @@ describe('Coin', function () {
 
         context('When unit is base unit', function () {
             it('should throw Error when the provided string is not a valid number', function () {
-                expect(() => cro.Coin('invalid', Coin.UNIT_BASE)).to.throw(
+                expect(() => new cro.coin.Coin('invalid', cro.coin.Coin.UNIT_BASE)).to.throw(
                     'Expected amount to be a base10 number represented as string',
                 );
             });
 
             it('should throw Error when the provided string is a floating number', function () {
-                expect(() => cro.Coin('1234.5678', Coin.UNIT_BASE)).to.throw('Expected base amount to be an integer');
+                expect(() => new cro.coin.Coin('1234.5678', cro.coin.Coin.UNIT_BASE)).to.throw(
+                    'Expected base amount to be an integer',
+                );
             });
 
             it('should throw Error when the provided string is not a base10 number', function () {
-                expect(() => cro.Coin('0xff', Coin.UNIT_BASE)).to.throw(
+                expect(() => new cro.coin.Coin('0xff', cro.coin.Coin.UNIT_BASE)).to.throw(
                     'Expected amount to be a base10 number represented as string',
                 );
             });
 
             it('should throw Error when the provided string is a negative integer', function () {
-                expect(() => cro.Coin('-1000', Coin.UNIT_BASE)).to.throw('Expected base amount to be positive');
+                expect(() => new cro.coin.Coin('-1000', cro.coin.Coin.UNIT_BASE)).to.throw(
+                    'Expected base amount to be positive',
+                );
             });
 
             it('should throw Error if the value exceed total supply', function () {
-                expect(() => cro.Coin('10000000000000000001', Coin.UNIT_BASE)).to.throw(
+                expect(() => new cro.coin.Coin('10000000000000000001', cro.coin.Coin.UNIT_BASE)).to.throw(
                     'Expected base amount to be within total supply',
                 );
             });
 
             it('should return a coins object of the provided string', function () {
                 const anyBaseValue = '1000';
-                const coins = cro.Coin(anyBaseValue, Coin.UNIT_BASE);
+                const coins = new cro.coin.Coin(anyBaseValue, cro.coin.Coin.UNIT_BASE);
 
                 expect(coins.toString()).to.eq(anyBaseValue);
             });
@@ -63,36 +66,38 @@ describe('Coin', function () {
 
         context('When unit is CRO', function () {
             it('should throw Error when the provided string is not a valid number', function () {
-                expect(() => cro.Coin('invalid', Coin.UNIT_CRO)).to.throw(
+                expect(() => new cro.coin.Coin('invalid', cro.coin.Coin.UNIT_CRO)).to.throw(
                     'Expected amount to be a base10 number represented as string',
                 );
             });
 
             it('should throw Error when the provided string is not a base10 number', function () {
-                expect(() => cro.Coin('0xff', Coin.UNIT_CRO)).to.throw(
+                expect(() => new cro.coin.Coin('0xff', cro.coin.Coin.UNIT_CRO)).to.throw(
                     'Expected amount to be a base10 number represented as string',
                 );
             });
 
             it('should throw Error when the provided string is a negative integer', function () {
-                expect(() => cro.Coin('-1000', Coin.UNIT_CRO)).to.throw('Expected CRO amount to be positive');
+                expect(() => new cro.coin.Coin('-1000', cro.coin.Coin.UNIT_CRO)).to.throw(
+                    'Expected CRO amount to be positive',
+                );
             });
 
             it('should throw Error when the provided string exceed 8 decimal places', function () {
-                expect(() => cro.Coin('1000.123456789', Coin.UNIT_CRO)).to.throw(
+                expect(() => new cro.coin.Coin('1000.123456789', cro.coin.Coin.UNIT_CRO)).to.throw(
                     'Expected CRO amount to have at most 8 decimal places',
                 );
             });
 
             it('should throw Error if the value exceed total supply', function () {
-                expect(() => cro.Coin('100000000001', Coin.UNIT_CRO)).to.throw(
+                expect(() => new cro.coin.Coin('100000000001', cro.coin.Coin.UNIT_CRO)).to.throw(
                     'Expected CRO amount to be within total supply',
                 );
             });
 
             it('should return a coins object of the provided string', function () {
                 const anyCROValue = '0.00001';
-                const coins = cro.Coin(anyCROValue, Coin.UNIT_CRO);
+                const coins = new cro.coin.Coin(anyCROValue, cro.coin.Coin.UNIT_CRO);
 
                 const expectedBaseValue = '1000';
                 expect(coins.toString()).to.eq(expectedBaseValue);
@@ -105,42 +110,44 @@ describe('Coin', function () {
             const testRunner = fuzzy(fuzzy.StringArg('1000'));
             testRunner(
                 function (arg) {
-                    expect(() => cro.coinFromBaseUnit(arg.value)).to.throw('Expected `amount` to be of type `string`');
+                    expect(() => cro.coin.Coin.fromBaseUnit(arg.value)).to.throw(
+                        'Expected `amount` to be of type `string`',
+                    );
                 },
                 { invalidArgsOnly: true },
             );
         });
 
         it('should throw Error when the provided string is not a valid number', function () {
-            expect(() => cro.coinFromBaseUnit('invalid')).to.throw(
+            expect(() => cro.coin.Coin.fromBaseUnit('invalid')).to.throw(
                 'Expected amount to be a base10 number represented as string,',
             );
         });
 
         it('should throw Error when the provided string is a floating number', function () {
-            expect(() => cro.coinFromBaseUnit('1234.5678')).to.throw('Expected base amount to be an integer');
-            expect(() => cro.coinFromBaseUnit('-1234.5678')).to.throw('Expected base amount to be an integer');
+            expect(() => cro.coin.Coin.fromBaseUnit('1234.5678')).to.throw('Expected base amount to be an integer');
+            expect(() => cro.coin.Coin.fromBaseUnit('-1234.5678')).to.throw('Expected base amount to be an integer');
         });
 
         it('should throw Error when the provided string is not a base10 number', function () {
-            expect(() => cro.coinFromBaseUnit('0xff')).to.throw(
+            expect(() => cro.coin.Coin.fromBaseUnit('0xff')).to.throw(
                 'Expected amount to be a base10 number represented as string',
             );
         });
 
         it('should throw Error when the provided string is a negative integer', function () {
-            expect(() => cro.coinFromBaseUnit('-1000')).to.throw('Expected base amount to be positive');
+            expect(() => cro.coin.Coin.fromBaseUnit('-1000')).to.throw('Expected base amount to be positive');
         });
 
         it('should throw Error if the value exceed total supply', function () {
-            expect(() => cro.coinFromBaseUnit('10000000000000000001')).to.throw(
+            expect(() => cro.coin.Coin.fromBaseUnit('10000000000000000001')).to.throw(
                 'Expected base amount to be within total supply',
             );
         });
 
         it('should return a coins object of the provided base unit string', function () {
             const anyBaseValue = '1000';
-            const coins = cro.coinFromBaseUnit(anyBaseValue);
+            const coins = cro.coin.Coin.fromBaseUnit(anyBaseValue);
 
             expect(coins.toString()).to.eq(anyBaseValue);
         });
@@ -151,46 +158,46 @@ describe('Coin', function () {
             const testRunner = fuzzy(fuzzy.StringArg('0.1'));
             testRunner(
                 function (arg) {
-                    expect(() => cro.coinFromCRO(arg.value)).to.throw('Expected `amount` to be of type `string`');
+                    expect(() => cro.coin.Coin.fromCRO(arg.value)).to.throw('Expected `amount` to be of type `string`');
                 },
                 { invalidArgsOnly: true },
             );
         });
 
         it('should throw Error when the provided string is not a valid number', function () {
-            expect(() => cro.coinFromCRO('invalid')).to.throw(
+            expect(() => cro.coin.Coin.fromCRO('invalid')).to.throw(
                 'Expected amount to be a base10 number represented as string',
             );
         });
 
         it('should throw Error when the provided string exceeds 8 decimal places', function () {
-            expect(() => cro.coinFromCRO('1000.123456789')).to.throw(
+            expect(() => cro.coin.Coin.fromCRO('1000.123456789')).to.throw(
                 'Expected CRO amount to have at most 8 decimal places',
             );
-            expect(() => cro.coinFromCRO('-1000.123456789')).to.throw(
+            expect(() => cro.coin.Coin.fromCRO('-1000.123456789')).to.throw(
                 'Expected CRO amount to have at most 8 decimal places',
             );
         });
 
         it('should throw Error when the provided string is not a base10 number', function () {
-            expect(() => cro.coinFromCRO('0xff')).to.throw(
+            expect(() => cro.coin.Coin.fromCRO('0xff')).to.throw(
                 'Expected amount to be a base10 number represented as string',
             );
         });
 
         it('should throw Error when the provided string is a negative integer', function () {
-            expect(() => cro.coinFromCRO('-1000')).to.throw('Expected CRO amount to be positive');
+            expect(() => cro.coin.Coin.fromCRO('-1000')).to.throw('Expected CRO amount to be positive');
         });
 
         it('should throw Error if the value exceed total supply', function () {
-            expect(() => cro.coinFromCRO('10000000000000000001')).to.throw(
+            expect(() => cro.coin.Coin.fromCRO('10000000000000000001')).to.throw(
                 'Expected CRO amount to be within total supply',
             );
         });
 
         it('should return a coins object of the provided CRO unit string', function () {
             const anyCROValue = '0.00001';
-            const coins = cro.coinFromCRO(anyCROValue);
+            const coins = cro.coin.Coin.fromCRO(anyCROValue);
 
             const expectedBaseValue = '1000';
             expect(coins.toString()).to.eq(expectedBaseValue);
@@ -199,11 +206,11 @@ describe('Coin', function () {
 
     describe('add', function () {
         fuzzyDescribe('should throw Error when the provided coins is not an instance of Coin', function (fuzzy) {
-            const anyValidCoin = cro.coinFromBaseUnit('1000');
+            const anyValidCoin = cro.coin.Coin.fromBaseUnit('1000');
             const testRunner = fuzzy(fuzzy.ObjArg(anyValidCoin));
             testRunner(
                 function (arg) {
-                    const anyCoin = cro.coinFromBaseUnit('1000');
+                    const anyCoin = cro.coin.Coin.fromBaseUnit('1000');
 
                     let expectedErrMsg: string;
                     if (arg.type === fuzzy.Object) {
@@ -218,23 +225,23 @@ describe('Coin', function () {
         });
 
         it('should throw Error when the result exceeds total supply', function () {
-            const totalSupply = Coin.getTotalSupply(CroSDK.Testnet);
-            const oneBaseUnit = cro.coinFromBaseUnit('1');
+            const totalSupply = cro.coin.Coin.TOTAL_SUPPLY;
+            const oneBaseUnit = cro.coin.Coin.fromBaseUnit('1');
 
             expect(() => totalSupply.add(oneBaseUnit)).to.throw('Adding two Coin together exceed total supply');
         });
 
         it('should return the sum of the two coins', function () {
-            const anyCoin = cro.coinFromBaseUnit('1000');
-            const anyAnotherCoin = cro.coinFromBaseUnit('10000000');
+            const anyCoin = cro.coin.Coin.fromBaseUnit('1000');
+            const anyAnotherCoin = cro.coin.Coin.fromBaseUnit('10000000');
 
             expect(anyCoin.add(anyAnotherCoin).toString()).to.eq('10001000');
         });
 
         it('should return a cro.coins without modifying the original coins', function () {
             const anyCoinValue = '1000';
-            const anyCoin = cro.coinFromBaseUnit(anyCoinValue);
-            const anyAnotherCoin = cro.coinFromBaseUnit('10000000');
+            const anyCoin = cro.coin.Coin.fromBaseUnit(anyCoinValue);
+            const anyAnotherCoin = cro.coin.Coin.fromBaseUnit('10000000');
 
             anyCoin.add(anyAnotherCoin);
             expect(anyCoin.toString()).to.eq(anyCoinValue);
@@ -243,11 +250,11 @@ describe('Coin', function () {
 
     describe('sub', function () {
         fuzzyDescribe('should throw Error when the provided coins is not an instance of Coin', function (fuzzy) {
-            const anyValidCoin = cro.coinFromBaseUnit('1000');
+            const anyValidCoin = cro.coin.Coin.fromBaseUnit('1000');
             const testRunner = fuzzy(fuzzy.ObjArg(anyValidCoin));
             testRunner(
                 function (arg) {
-                    const anyCoin = cro.coinFromBaseUnit('1000');
+                    const anyCoin = cro.coin.Coin.fromBaseUnit('1000');
 
                     let expectedErrMsg: string;
                     if (arg.type === fuzzy.Object) {
@@ -262,23 +269,23 @@ describe('Coin', function () {
         });
 
         it('should throw Error when the result is negative', function () {
-            const oneBaseUnit = cro.coinFromBaseUnit('1');
-            const twoBaseUnit = cro.coinFromBaseUnit('2');
+            const oneBaseUnit = cro.coin.Coin.fromBaseUnit('1');
+            const twoBaseUnit = cro.coin.Coin.fromBaseUnit('2');
 
             expect(() => oneBaseUnit.sub(twoBaseUnit)).to.throw('Subtracting the Coin results in negation Coin');
         });
 
         it('should return the result of subtracting Coin with the provided Coin', function () {
-            const anyCoin = cro.coinFromBaseUnit('10006000');
-            const anyAnotherCoin = cro.coinFromBaseUnit('1000');
+            const anyCoin = cro.coin.Coin.fromBaseUnit('10006000');
+            const anyAnotherCoin = cro.coin.Coin.fromBaseUnit('1000');
 
             expect(anyCoin.sub(anyAnotherCoin).toString()).to.eq('10005000');
         });
 
         it('should return a cro.coins without modifying the original coins', function () {
             const anyCoinValue = '10006000';
-            const anyCoin = cro.coinFromBaseUnit(anyCoinValue);
-            const anyAnotherCoin = cro.coinFromBaseUnit('1000');
+            const anyCoin = cro.coin.Coin.fromBaseUnit(anyCoinValue);
+            const anyAnotherCoin = cro.coin.Coin.fromBaseUnit('1000');
 
             anyCoin.sub(anyAnotherCoin);
             expect(anyCoin.toString()).to.eq(anyCoinValue);
@@ -287,7 +294,7 @@ describe('Coin', function () {
 
     describe('toCosmosCoin', function () {
         it('should return the Cosmos Coin object', function () {
-            const anyCoin = cro.coinFromBaseUnit('1000');
+            const anyCoin = cro.coin.Coin.fromBaseUnit('1000');
             expect(anyCoin.toCosmosCoin()).to.deep.eq({
                 amount: '1000',
                 denom: CroSDK.Testnet.coin.baseDenom,
@@ -297,10 +304,10 @@ describe('Coin', function () {
 
     describe('toString', function () {
         fuzzyDescribe('should throw Error when the provided unit is not a string', function (fuzzy) {
-            const testRunner = fuzzy(fuzzy.optional(fuzzy.String)(Coin.UNIT_BASE));
+            const testRunner = fuzzy(fuzzy.optional(fuzzy.String)(cro.coin.Coin.UNIT_BASE));
             testRunner(
                 function (arg) {
-                    const anyCoin = cro.coinFromBaseUnit('1000');
+                    const anyCoin = cro.coin.Coin.fromBaseUnit('1000');
                     expect(() => anyCoin.toString(arg.value)).to.throw('Expected argument to be of type `string`');
                 },
                 { invalidArgsOnly: true },
@@ -308,39 +315,39 @@ describe('Coin', function () {
         });
 
         it('should throw Error when the provided unit is invalid', function () {
-            const anyCoin = cro.coinFromBaseUnit('1000');
+            const anyCoin = cro.coin.Coin.fromBaseUnit('1000');
 
             expect(() => anyCoin.toString('invalid' as any)).to.throw('Expected string to be one of the Coin units');
         });
 
         it('should return the base unit when no unit is provided', function () {
             const anyBaseValue = '1000';
-            const coins = cro.coinFromBaseUnit(anyBaseValue);
+            const coins = cro.coin.Coin.fromBaseUnit(anyBaseValue);
 
             expect(coins.toString()).to.eq(anyBaseValue);
         });
 
         it('should return the base unit when unit is base', function () {
             const anyBaseValue = '1000';
-            const coins = cro.coinFromBaseUnit(anyBaseValue);
+            const coins = cro.coin.Coin.fromBaseUnit(anyBaseValue);
 
-            expect(coins.toString(Coin.UNIT_BASE)).to.eq(anyBaseValue);
+            expect(coins.toString(cro.coin.Coin.UNIT_BASE)).to.eq(anyBaseValue);
         });
 
         it('should return the CRO value when unit is CRO', function () {
             const anyBaseValue = '1000';
-            const coins = cro.coinFromBaseUnit(anyBaseValue);
+            const coins = cro.coin.Coin.fromBaseUnit(anyBaseValue);
 
             const expectedCROValue = '0.00001';
-            expect(coins.toString(Coin.UNIT_CRO)).to.eq(expectedCROValue);
+            expect(coins.toString(cro.coin.Coin.UNIT_CRO)).to.eq(expectedCROValue);
         });
 
         it('should return the CRO value when unit is CRO and has 8 decimal places', function () {
             const anyBaseValue = '12345678';
-            const coins = cro.coinFromBaseUnit(anyBaseValue);
+            const coins = cro.coin.Coin.fromBaseUnit(anyBaseValue);
 
             const expectedCROValue = '0.12345678';
-            expect(coins.toString(Coin.UNIT_CRO)).to.eq(expectedCROValue);
+            expect(coins.toString(cro.coin.Coin.UNIT_CRO)).to.eq(expectedCROValue);
         });
     });
 });
--- lib/src/core/cro.ts
+++ lib/src/core/cro.ts
@@ -1,12 +1,13 @@
-import ow from 'ow';
 import { Network } from '../network/network';
-import { Coin, Units } from '../coin/coin';
-import { RawTransaction } from '../transaction/raw';
-import { owCroSDKInitParams } from './ow.types';
+import { coin } from '../coin/coin';
+
+export const croSDK = function (config: InitConfigurations) {
+    return {
+        coin: coin(config),
+    };
+};
 
 export class CroSDK {
-    public readonly configs: InitConfigurations;
-
     public static Testnet: Network = {
         chainId: 'testnet-croeseid-1',
         addressPrefix: 'tcro',
@@ -19,60 +20,6 @@ export class CroSDK {
             account: 0,
         },
     };
-
-    /**
-     * Constructor to create a CroSDK
-     * @param {InitConfigurations} configs SDK initialization configurations
-     * @throws {Error} amount or unit is invalid
-     * @returns {CroSDK}
-     */
-    constructor(configs: InitConfigurations) {
-        ow(configs, 'configs', owCroSDKInitParams);
-        this.configs = configs;
-    }
-
-    /**
-     * Create a network aware Coin object from sdk config
-     * @param {string} amount coins value in specified unit
-     * @param {Units} unit unit for the specified amount
-     * @returns {Coin}
-     * @throws {Error} base value is invalid
-     * @memberof CroSDK
-     */
-    public Coin(amount: string, unit: Units): Coin {
-        return new Coin(amount, unit, this.configs.network);
-    }
-
-    /**
-     * Create a Coin from the base unit
-     * @param {string} baseValue coins value in base unit
-     * @returns {Coin}
-     * @throws {Error} base value is invalid
-     * @memberof Cro
-     */
-    public coinFromBaseUnit(baseValue: string): Coin {
-        return new Coin(baseValue, Units.BASE, this.configs.network);
-    }
-
-    /**
-     * Create a Coin from CRO unit
-     * @param {string} croValue coins value in CRO unit
-     * @returns {Coin}
-     * @throws {Error} cro value is invalid
-     * @memberof Cro
-     */
-    public coinFromCRO(croValue: string): Coin {
-        return new Coin(croValue, Units.CRO, this.configs.network);
-    }
-
-    /**
-     * Create a new network aware RawTransaction from sdk configs
-     * @returns {RawTransaction}
-     * @throws {Error} when options is invalid
-     */
-    public RawTransaction(): RawTransaction {
-        return new RawTransaction({ network: this.configs.network });
-    }
 }
 
 export type InitConfigurations = {
