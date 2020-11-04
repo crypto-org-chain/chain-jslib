0036aa71c9ffa1ae306973ba149103de5fe8bd08
Eddy <eddy.wanny@crypto.com>
2020-11-04 19:58:16 +0300

    clean up unused stuff

--- lib/src/cosmos/v1beta1/types/msg.ts
+++ lib/src/cosmos/v1beta1/types/msg.ts
@@ -7,23 +7,7 @@ export type Msg = {
         [key: string]: any;
     };
 };
-// | MsgWithdrawDelegatorReward;
-
 export const owMsg = () =>
     owStrictObject().partialShape({
         typeUrl: ow.string,
     });
-
-// export type distribution = {
-//     v1beta1: {
-//         MsgWithdrawDelegatorReward: MsgWithdrawDelegatorReward;
-//     };
-// };
-//
-// type MsgWithdrawDelegatorReward = {
-//     typeUrl: '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward';
-//     value: {
-//         delegatorAddress: string;
-//         validatorAddress: string;
-//     };
-// };
