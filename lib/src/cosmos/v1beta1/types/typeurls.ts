525a99eb3782a81f474354941eba8f5cee2586c4
hitesh.goel <hitesh.goel@SGMAC0008.local>
2020-11-05 16:01:01 +0800

    - typeurl added - MsgDelegate - OwTypes

--- lib/src/cosmos/v1beta1/types/typeurls.ts
+++ lib/src/cosmos/v1beta1/types/typeurls.ts
@@ -13,6 +13,7 @@ export const typeUrlMappings: {
     '/cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward': cosmos.distribution.v1beta1.MsgWithdrawDelegatorReward,
     '/cosmos.staking.v1beta1.MsgCreateValidator': cosmos.staking.v1beta1.MsgCreateValidator,
     '/cosmos.staking.v1beta1.MsgEditValidator': cosmos.staking.v1beta1.MsgEditValidator,
+    '/cosmos.staking.v1beta1.MsgDelegate': cosmos.staking.v1beta1.MsgDelegate,
 };
 
 export interface GeneratedType {
---
+++ lib/src/transaction/msg/MsgDelegate.ts
@@ -0,0 +1,71 @@
+import ow from 'ow';
+import { Msg } from '../../cosmos/v1beta1/types/msg';
+import { Message } from './Message';
+import { owMsgEditValidatorOptions } from './ow.types';
+import { InitConfigurations } from '../../core/cro';
+import { isValidAddress, AddressType } from '../../utils/address';
+import { ICoin } from '../../coin/coin';
+
+export const msgDelegate = function (config: InitConfigurations) {
+    return class MsgDelegate implements Message {
+        /** MsgDelegate delegatorAddress. */
+        public delegatorAddress: string;
+
+        /** MsgDelegate validatorAddress. */
+        public validatorAddress: string;
+
+        /** MsgDelegate amount. */
+        public amount: ICoin
+
+        /**
+         * Constructor to create a new MsgSend
+         * @param {IMsgDelegate} options
+         * @returns {MsgDelegate}
+         * @throws {Error} when options is invalid
+         */
+        constructor(options: IMsgDelegate) {
+            ow(options, 'options', owMsgEditValidatorOptions);
+            this.delegatorAddress = options.delegatorAddress;
+            this.validatorAddress = options.validatorAddress;
+            this.amount = options.amount;
+            this.validateAddresses();
+        }
+
+        /**
+         * Returns the raw Msg representation of MsgDelegate
+         * @returns {Msg}
+         */
+        toRawMsg(): Msg {
+            const cosmosCoin = this.amount.toCosmosCoin();
+            return {
+                typeUrl: '/cosmos.staking.v1beta1.MsgDelegate',
+                value: {
+                    delegatorAddress: this.delegatorAddress,
+                    validatorAddress: this.validatorAddress,
+                    amount: {
+                        denom: cosmosCoin.denom,
+                        amount: cosmosCoin.amount,
+                    }
+                },
+            };
+        }
+
+        validateAddresses(): void {
+            const { network } = config;
+
+            if (!isValidAddress({ network, address: this.validatorAddress, type: AddressType.VALIDATOR })) {
+                throw new TypeError('Provided `validatorAddress` doesnt match network selected');
+            }
+
+            if (!isValidAddress({ network, address: this.delegatorAddress, type: AddressType.USER })) {
+                throw new TypeError('Provided `delegatorAddress` doesnt match network selected');
+            }
+        }
+    };
+};
+
+export type IMsgDelegate = {
+    delegatorAddress: string;
+    validatorAddress: string;
+    amount: ICoin;
+};
