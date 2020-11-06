6431c582ee403983391ed7c59afb39174a82acd9
Eddy <eddy.wanny@crypto.com>
2020-11-06 10:09:54 +0300

    Export CroSDK & CroNetwork

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -2,6 +2,7 @@ import Big from 'big.js';
 import { HDKey } from './hdkey/hdkey';
 import { Secp256k1KeyPair } from './keypair/secp256k1';
 import utils from './utils';
+import { CroNetwork, CroSDK } from './core/cro';
 
 // The maximum number of decimal places of the results of operations involving division
 // By default it is 20. Here we explicitly set it to 20 for correctness.
@@ -11,4 +12,6 @@ export default {
     utils,
     HDKey,
     Secp256k1KeyPair,
+    CroSDK,
+    CroNetwork,
 };
