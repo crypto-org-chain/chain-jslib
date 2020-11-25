02fedc60b33de7f26b2c2a75e3f097c5e47342d5
Eddy <eddy.wanny@crypto.com>
2020-11-25 10:20:44 +0300

    added MsgDeposit

--- lib/src/transaction/msg/ow.types.ts
+++ lib/src/transaction/msg/ow.types.ts
@@ -1,10 +1,16 @@
 import ow from 'ow';
 import { owCoin } from '../../coin/ow.types';
-import { owStrictObject } from '../../ow.types';
+import { owBig, owStrictObject } from '../../ow.types';
 
 export const owMsgSendOptions = owStrictObject().exactShape({
     fromAddress: ow.string,
     toAddress: ow.string,
+    amount: owCoin(),
+});
+
+export const owMsgDepositOptions = owStrictObject().exactShape({
+    depositor: ow.string,
+    proposalId: owBig(),
     amount: owCoin(),
 });
 
