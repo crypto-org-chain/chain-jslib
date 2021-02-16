a33a2b8177431190c6a7c28c61b2e1335abc588e
Eddy CPC <eddy.wanny@crypto.com>
2021-02-16 10:29:57 +0300

    Plan v0.0.10 release - Support for LegacyAmino for undelegate transactions

--- lib/src/index.ts
+++ lib/src/index.ts
@@ -4,6 +4,9 @@ import { Secp256k1KeyPair } from './keypair/secp256k1';
 import utils from './utils';
 import { CroNetwork, CroSDK } from './core/cro';
 import { Units } from './coin/coin';
+import { cosmos } from './cosmos/v1beta1/codec';
+// eslint-disable-next-line no-undef
+import Tx = cosmos.tx.v1beta1.Tx;
 
 // The maximum number of decimal places of the results of operations involving division
 // By default it is 20. Here we explicitly set it to 20 for correctness.
@@ -16,6 +19,7 @@ const _ = {
     CroSDK,
     CroNetwork,
     Units,
+    Tx,
 };
 
 export = _;
