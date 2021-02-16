1f8f3bef6285e2cc237d2d9687a1356079951860
jonghwan <jonghwan@crypto.com>
2021-02-16 12:40:43 +0900

    Problem: missing Undelegate for amino json (fix #146)
    add unit test
    
    fix lint
    
    rename

--- lib/src/transaction/msg/staking/MsgUndelegate.spec.ts
+++ lib/src/transaction/msg/staking/MsgUndelegate.spec.ts
@@ -8,6 +8,7 @@ import { Secp256k1KeyPair } from '../../../keypair/secp256k1';
 import { Bytes } from '../../../utils/bytes/bytes';
 import { Units } from '../../../coin/coin';
 import { CroSDK } from '../../../core/cro';
+import * as legacyAmino from '../../../cosmos/amino';
 
 const cro = CroSDK({
     network: {
@@ -130,5 +131,31 @@ describe('Testing MsgUndelegate (Unbonding)', function () {
         expect(() => new cro.staking.MsgUndelegate(params3)).to.throw(
             'Invalid checksum for tcro165tzcrh2yl83g8qeqxueg2g5gzgu57y3fe3kc',
         );
+    });
+
+    describe('Testing MsgUndelegate Json', function () {
+        it('Test MsgUndelegate conversion for amino json', function () {
+            const coin = new cro.Coin('12000500', Units.BASE);
+
+            const MsgUndelegate = new cro.staking.MsgUndelegate({
+                delegatorAddress: 'tcro165tzcrh2yl83g8qeqxueg2g5gzgu57y3fe3kc3',
+                validatorAddress: 'tcrocncl1j7pej8kplem4wt50p4hfvndhuw5jprxxxtenvr',
+                amount: coin,
+            });
+
+            const rawMsg: legacyAmino.Msg = {
+                type: 'cosmos-sdk/MsgUndelegate',
+                value: {
+                    delegator_address: 'tcro165tzcrh2yl83g8qeqxueg2g5gzgu57y3fe3kc3',
+                    validator_address: 'tcrocncl1j7pej8kplem4wt50p4hfvndhuw5jprxxxtenvr',
+                    amount: {
+                        denom: 'basetcro',
+                        amount: '12000500',
+                    },
+                },
+            };
+
+            expect(MsgUndelegate.toRawAminoMsg()).to.eqls(rawMsg);
+        });
     });
 });
