f1179ce63eed5aba08f985ef93e7673b8150498d
hitesh.goel <hitesh.goel@SGMAC0008.local>
2021-04-21 17:11:38 +0530

    Saving last progress

--- lib/src/cosmos/v1beta1/types/typeurls.ts
+++ lib/src/cosmos/v1beta1/types/typeurls.ts
@@ -3,7 +3,7 @@
 // Copyright © 2020 Simon Warta (licensed under the Apache License, Version 2.0)
 // Modifications Copyright (c) 2018 - 2020, Foris Limited (licensed under the Apache License, Version 2.0)
 import protobuf from 'protobufjs';
-import { cosmos } from '../codec';
+import { cosmos, google } from '../codec';
 
 export const typeUrlMappings: {
     [key: string]: GeneratedType;
@@ -19,9 +19,11 @@ export const typeUrlMappings: {
     '/cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission':
         cosmos.distribution.v1beta1.MsgWithdrawValidatorCommission,
     '/cosmos.crypto.ed25519.PubKey': cosmos.crypto.ed25519.PubKey,
+    '/cosmos.crypto.secp256k1.PubKey': cosmos.crypto.secp256k1.PubKey,
     '/cosmos.gov.v1beta1.MsgDeposit': cosmos.gov.v1beta1.MsgDeposit,
     '/cosmos.gov.v1beta1.MsgVote': cosmos.gov.v1beta1.MsgVote,
     '/cosmos.gov.v1beta1.MsgSubmitProposal': cosmos.gov.v1beta1.MsgSubmitProposal,
+    '/google.protobuf.Any': google.protobuf.Any,
 };
 
 export interface GeneratedType {
