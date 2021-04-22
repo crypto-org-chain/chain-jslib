cb97cf42cbd28c92ebe5afbed0ab9ee0c6d8f6ec
hitesh.goel <hitesh.goel@SGMAC0008.local>
2021-04-23 03:47:35 +0530

    eslint fix

--- lib/src/utils/txDecoder.ts
+++ lib/src/utils/txDecoder.ts
@@ -1,17 +1,19 @@
-import { Registry } from '@cosmjs/proto-signing'
-import { typeUrlMappings } from '../cosmos/v1beta1/types/typeurls';
+import { Registry } from '@cosmjs/proto-signing';
 import { toBase64, fromBase64 } from '@cosmjs/encoding';
-import { Bytes } from './bytes/bytes';
-import { cosmos } from '../cosmos/v1beta1/codec/generated/codecimpl';
-import { TxRaw, AuthInfo, TxBody, SignerInfo } from '@cosmjs/proto-signing/build/codec/cosmos/tx/v1beta1/tx';
-import { Tx } from '@cosmjs/proto-signing/build/codec/cosmos/tx/v1beta1/tx';
+import { TxRaw, AuthInfo, TxBody, SignerInfo, Tx } from '@cosmjs/proto-signing/build/codec/cosmos/tx/v1beta1/tx';
+
 import * as snakecaseKeys from 'snakecase-keys';
+import { cosmos } from '../cosmos/v1beta1/codec/generated/codecimpl';
+import { Bytes } from './bytes/bytes';
+import { typeUrlMappings } from '../cosmos/v1beta1/types/typeurls';
 
 export class TxDecoder {
-
     private protoDecodedTxRaw!: TxRaw;
+
     private libDecodedTxBody!: TxBody;
+
     private libDecodedAuthInfo!: AuthInfo;
+
     private libDecodedSignatures!: Uint8Array[];
 
     private readonly cosmJSRegistry = new Registry(Object.entries(typeUrlMappings));
@@ -25,31 +27,30 @@ export class TxDecoder {
         this.libDecodedAuthInfo = Object.create({});
         this.libDecodedSignatures = Object.create(null);
     }
+
     isValidHex(h: string) {
-        var re = /[0-9A-Fa-f]/g;
+        const re = /[0-9A-Fa-f]/g;
         if (!re.test(h)) {
-            throw new TypeError("Invalid Hex provided.");
+            throw new TypeError('Invalid Hex provided.');
         }
-        return;
     }
 
     /**
      * @name fromHex()
-     * @param txHex 
+     * @param txHex
      * @returns TxDecode
      */
     public fromHex(txHex: string): TxDecoder {
-
         if (!txHex) {
             throw new TypeError(`Received malformed transaction hex.`);
         }
         const sanitisedTxHex = Bytes.clean0x(txHex);
         try {
-            this.isValidHex(sanitisedTxHex)
-            console.log(sanitisedTxHex)
+            this.isValidHex(sanitisedTxHex);
+            console.log(sanitisedTxHex);
             const encodedTxBytes = Bytes.fromHexString(sanitisedTxHex).toUint8Array();
-            let decodedTxRaw = cosmos.tx.v1beta1.TxRaw.decode(encodedTxBytes)!;
-            let libDecodedTx = Tx.decode(encodedTxBytes);
+            const decodedTxRaw = cosmos.tx.v1beta1.TxRaw.decode(encodedTxBytes)!;
+            const libDecodedTx = Tx.decode(encodedTxBytes);
             this.protoDecodedTxRaw = decodedTxRaw;
             this.libDecodedSignatures = libDecodedTx.signatures;
             this.libDecodedAuthInfo = libDecodedTx.authInfo!;
@@ -59,7 +60,7 @@ export class TxDecoder {
 
             return this;
         } catch (error) {
-            console.error(error)
+            console.error(error);
             throw new TypeError(`Error decoding provided transaction hex.`);
         }
     }
@@ -72,13 +73,13 @@ export class TxDecoder {
             throw new TypeError(`Cannot convert ${this.protoDecodedTxRaw} to JSON.`);
         }
 
-        let txObject = {
+        const txObject = {
             tx: {
                 body: Object.create({}),
                 authInfo: Object.create({}),
                 signatures: Object.create([[]]),
-            }
-        }
+            },
+        };
 
         txObject.tx.body = this.getTxBodyJson(this.libDecodedTxBody);
         txObject.tx.signatures = this.getSignaturesJson(this.libDecodedSignatures);
@@ -99,10 +100,10 @@ export class TxDecoder {
         obj = { ...parsedTxBody };
         obj.messages = txBody.messages.map(({ typeUrl, value }) => {
             if (!typeUrl) {
-                throw new Error("Missing type_url in Any");
+                throw new Error('Missing type_url in Any');
             }
             if (!value) {
-                throw new Error("Missing value in Any");
+                throw new Error('Missing value in Any');
             }
             const decodedParams = this.cosmJSRegistry.decode({ typeUrl, value });
             return { typeUrl, ...decodedParams };
@@ -121,15 +122,14 @@ export class TxDecoder {
 
     private getAuthInfoJson(authInfo: AuthInfo) {
         let obj = Object.create({});
-        let authInfoStringified = JSON.stringify(AuthInfo.toJSON(authInfo));
+        const authInfoStringified = JSON.stringify(AuthInfo.toJSON(authInfo));
 
-        let libParsedAuthInfo = JSON.parse(authInfoStringified);
+        const libParsedAuthInfo = JSON.parse(authInfoStringified);
         obj = { ...libParsedAuthInfo };
 
         if (authInfo.signerInfos) {
             obj.signerInfos = authInfo.signerInfos.map((e) => (e ? this.getSignerInfoJson(e) : undefined));
-        }
-        else {
+        } else {
             obj.signerInfos = [];
         }
 
@@ -140,19 +140,20 @@ export class TxDecoder {
         let obj = Object.create({});
 
         const stringifiedSignerInfo = JSON.stringify(SignerInfo.toJSON(signerInfo) as string);
-        let libParsedSignerInfo = JSON.parse(stringifiedSignerInfo);
-        let decodedPubkey: cosmos.crypto.ed25519.PubKey | cosmos.crypto.secp256k1.PubKey = this.cosmJSRegistry.decode(
+        const libParsedSignerInfo = JSON.parse(stringifiedSignerInfo);
+        const decodedPubkey: cosmos.crypto.ed25519.PubKey | cosmos.crypto.secp256k1.PubKey = this.cosmJSRegistry.decode(
             {
                 typeUrl: libParsedSignerInfo.publicKey?.typeUrl!,
-                value: fromBase64(libParsedSignerInfo.publicKey?.value!)
-            });
+                value: fromBase64(libParsedSignerInfo.publicKey?.value!),
+            },
+        );
 
         obj = { ...libParsedSignerInfo };
-        obj.publicKey = { typeUrl: libParsedSignerInfo.publicKey?.typeUrl!, key: toBase64(decodedPubkey.key) }
+        obj.publicKey = { typeUrl: libParsedSignerInfo.publicKey?.typeUrl!, key: toBase64(decodedPubkey.key) };
 
         return obj;
     }
 
     // transforms `type_url` to `@type` to match GoLang's TxDecoder JSON output
-    private typeUrlTransformer = (str: string) => str.replace(/type_url/g, '@type')
+    private typeUrlTransformer = (str: string) => str.replace(/type_url/g, '@type');
 }
