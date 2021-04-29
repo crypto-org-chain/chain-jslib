9da0db0019f44215758c60757fc6e9b2d39589b2
hitesh.goel <hitesh.goel@SGMAC0008.local>
2021-04-29 18:26:53 +0530

    #26: Added a throw unit test case

--- lib/src/transaction/signable.spec.ts
+++ lib/src/transaction/signable.spec.ts
@@ -168,6 +168,18 @@ describe('SignableTransaction', function () {
     });
 
     describe('toCosmosJSON', function () {
+
+        it('should throw', function () {
+            const { params: anyParams } = SignableTransactionParamsSuiteFactory.build();
+            const anyTx = new SignableTransaction(anyParams);
+            // @ts-ignore
+            anyTx['txRaw']['authInfoBytes'] = undefined
+            
+            expect(() => {
+                anyTx.toCosmosJSON();
+            }).to.throw('Error converting SignableTransaction to Cosmos compatible JSON.`');
+        });
+
         it('should not throw', function () {
             const anyTx = anySignableTransaction();
             expect(() => {
