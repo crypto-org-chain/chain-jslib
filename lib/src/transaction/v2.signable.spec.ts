2b613f6240efb08161b70435d57f7dba21676240
cdc-Hitesh <hitesh.goel@crypto.com>
2021-07-14 19:32:01 +0530

    V2 Signable added unit tests

--- lib/src/transaction/v2.signable.spec.ts
+++ lib/src/transaction/v2.signable.spec.ts
@@ -4,6 +4,8 @@ import { fuzzyDescribe } from '../test/mocha-fuzzy/suite';
 import { SignableTransactionParamsSuiteFactoryV2 } from './test';
 import { SignableTransactionV2 } from './v2.signable';
 import { Bytes } from '../utils/bytes/bytes';
+import { CroSDK, CroNetwork } from '../core/cro';
+import { typeUrlToMsgClassMapping, COSMOS_MSG_TYPEURL } from './common/constants/typeurl';
 
 const anySignableTransaction = (): SignableTransactionV2 => {
     const { params: anyParams } = SignableTransactionParamsSuiteFactoryV2.build();
@@ -165,7 +167,48 @@ describe('SignableTransaction', function () {
             expect(anyTx.isIndexSigned(anyValidIndex)).to.eq(true);
         });
     });
+    describe('check `typeUrlToMsgClassMapping`', function () {
+        const croSdk = CroSDK({ network: CroNetwork.TestnetCroeseid3 });
 
+        [
+            COSMOS_MSG_TYPEURL.MsgSend,
+            COSMOS_MSG_TYPEURL.distribution.MsgFundCommunityPool,
+            COSMOS_MSG_TYPEURL.distribution.MsgSetWithdrawAddress,
+            COSMOS_MSG_TYPEURL.MsgWithdrawDelegatorReward,
+            COSMOS_MSG_TYPEURL.MsgWithdrawValidatorCommission,
+            COSMOS_MSG_TYPEURL.MsgBeginRedelegate,
+            COSMOS_MSG_TYPEURL.MsgCreateValidator,
+            COSMOS_MSG_TYPEURL.MsgDelegate,
+            COSMOS_MSG_TYPEURL.MsgEditValidator,
+            COSMOS_MSG_TYPEURL.MsgUndelegate,
+            COSMOS_MSG_TYPEURL.MsgDeposit,
+            COSMOS_MSG_TYPEURL.MsgVote,
+            COSMOS_MSG_TYPEURL.MsgSubmitProposal,
+            COSMOS_MSG_TYPEURL.gov.TextProposal,
+            COSMOS_MSG_TYPEURL.upgrade.CancelSoftwareUpgradeProposal,
+            COSMOS_MSG_TYPEURL.upgrade.CommunityPoolSpendProposal,
+            COSMOS_MSG_TYPEURL.upgrade.ParameterChangeProposal,
+            COSMOS_MSG_TYPEURL.upgrade.SoftwareUpgradeProposal,
+            COSMOS_MSG_TYPEURL.ibc.MsgTransfer,
+            COSMOS_MSG_TYPEURL.ibc.MsgCreateClient,
+            COSMOS_MSG_TYPEURL.ibc.MsgUpdateClient,
+            COSMOS_MSG_TYPEURL.ibc.MsgUpgradeClient,
+            COSMOS_MSG_TYPEURL.ibc.MsgSubmitMisbehaviour,
+            COSMOS_MSG_TYPEURL.nft.MsgIssueDenom,
+            COSMOS_MSG_TYPEURL.nft.MsgMintNFT,
+            COSMOS_MSG_TYPEURL.nft.MsgEditNFT,
+            COSMOS_MSG_TYPEURL.nft.MsgTransferNFT,
+            COSMOS_MSG_TYPEURL.nft.MsgBurnNFT,
+        ].forEach((typeUrl) => {
+            const classInstance = typeUrlToMsgClassMapping(croSdk, typeUrl);
+            expect(() => classInstance).to.not.throw();
+            expect(classInstance).to.haveOwnProperty('fromCosmosMsgJSON');
+        });
+
+        expect(() => typeUrlToMsgClassMapping(croSdk, 'unsupported_type_url')).to.throw(
+            'unsupported_type_url not supported.',
+        );
+    });
     describe('toCosmosJSON', function () {
         it('should throw', function () {
             const { params: anyParams } = SignableTransactionParamsSuiteFactoryV2.build();
@@ -188,7 +231,7 @@ describe('SignableTransaction', function () {
         it('should create correct JSON', function () {
             const anyTx = anySignableTransaction();
             const parsedCosmosJson = JSON.parse(anyTx.toCosmosJSON());
-            // { "body": { "messages": [{ "@type": "/cosmos.bank.v1beta1.MsgSend", "amount": [{ "denom": "basetcro", "amount": "3478499933290496" }], "from_address": "tcro1x07kkkepfj2hl8etlcuqhej7jj6myqrp48y4hg", "to_address": "tcro184lta2lsyu47vwyp2e8zmtca3k5yq85p6c4vp3" }], "memo": "", "timeout_height": "0", "extension_options": [], "non_critical_extension_options": [] }, "auth_info": { "signer_infos": [{ "public_key": { "@type": "/cosmos.crypto.secp256k1.PubKey", "key": "Ap/w6zWJiX6QCKLTt6jLM1sFJsUmBWaS6VUi7zxqqb0V" }, "mode_info": { "single": { "mode": "SIGN_MODE_DIRECT" } }, "sequence": "794129105682432" }], "fee": { "amount": [], "gas_limit": "8105066556817408", "payer": "", "granter": "" } }, "signatures": [""] }
+            // { "body": { "messages": [{ "@type": "/cosmos.bank.v1beta1. ", "amount": [{ "denom": "basetcro", "amount": "3478499933290496" }], "from_address": "tcro1x07kkkepfj2hl8etlcuqhej7jj6myqrp48y4hg", "to_address": "tcro184lta2lsyu47vwyp2e8zmtca3k5yq85p6c4vp3" }], "memo": "", "timeout_height": "0", "extension_options": [], "non_critical_extension_options": [] }, "auth_info": { "signer_infos": [{ "public_key": { "@type": "/cosmos.crypto.secp256k1.PubKey", "key": "Ap/w6zWJiX6QCKLTt6jLM1sFJsUmBWaS6VUi7zxqqb0V" }, "mode_info": { "single": { "mode": "SIGN_MODE_DIRECT" } }, "sequence": "794129105682432" }], "fee": { "amount": [], "gas_limit": "8105066556817408", "payer": "", "granter": "" } }, "signatures": [""] }
             const expectedTxJSON = `{"body":{"messages":[{"@type":"/cosmos.bank.v1beta1.MsgSend","amount":[{"denom":"basetcro","amount":"1200050000000000"}],"from_address":"tcro165tzcrh2yl83g8qeqxueg2g5gzgu57y3fe3kc3","to_address":"tcro184lta2lsyu47vwyp2e8zmtca3k5yq85p6c4vp3"}],"memo":"","timeout_height":"0","extension_options":[],"non_critical_extension_options":[]},"auth_info":{"signer_infos":[{"public_key":{"@type":"/cosmos.crypto.secp256k1.PubKey","key":"A/0NVgtsSqHKFnIdA5oZKGfDRX4Z2tVT7bmOe6iLFZwn"},"mode_info":{"single":{"mode":"SIGN_MODE_DIRECT"}},"sequence":"2"}],"fee":{"amount":[],"gas_limit":"200000","payer":"","granter":""}},"signatures":[""]}`;
             const anyTxJSONStr = anyTx.toCosmosJSON();
             expect(anyTxJSONStr).to.deep.equal(expectedTxJSON);
